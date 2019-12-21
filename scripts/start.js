c55d3b01397e58e24bb6dc0c17edd33165c6f3f5
AlexanderKargin <kokargin.99@gmail.com>
2019-12-21 14:27:42 +0500

    asdas

--- scripts/start.js
+++ scripts/start.js
@@ -2,5 +2,5 @@ import { app } from '../src/server';
 
 const apiPort = process.env.EXPRESS_PORT || 5000;
 
-app.listen(apiPort, '0.0.0.0');
+app.listen(apiPort);
 console.log(`API listening on port: ${apiPort}`);
