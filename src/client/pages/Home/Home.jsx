8cc56f7168499423abefecd2f492889b7c61c245
vaspahomov <vas2142553@gmail.com>
2019-12-21 15:04:10 +0500

    d

--- src/client/pages/Home/Home.jsx
+++ src/client/pages/Home/Home.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 export class Home extends React.Component {
-    state = {login: '', password: '', error: null};
+    constructor() {
+        super();
+        this.state = {token: ''};
+    }
 
     submitStudent = () => {
 
     };
 
     submitTeacher = () => {
-
+        fetch('/teacher').then(x => {console.log(x.json())})
     };
 
-    onChange = () => {
-
+    onChange = (event) => {
+        this.setState({token: event.target.value});
     };
 
     render() {
@@ -20,10 +23,10 @@ export class Home extends React.Component {
             <main className="main-content">
                 <h1 className="title">Quizer</h1>
                 <div className="buttons">
-                    <button className="create">Create quiz</button>
+                    <button className="create" onClick={this.submitTeacher}>Create quiz</button>
                     <form>
                         <input type="submit" id="join" className="join" value="Join quiz" onClick={this.submitStudent}/>
-                        <input type="text" required placeholder="Quiz ID" onChange={this.onChange}/>
+                        <input type="text" value={this.state.token} required placeholder="Quiz ID" onChange={this.onChange}/>
                     </form>
                 </div>
             </main>
